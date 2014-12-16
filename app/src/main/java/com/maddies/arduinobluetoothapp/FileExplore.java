987288c7d42096ad7cdef9536e682cd5fce5d31d
Madgijs <Madgijs@users.noreply.github.com>
2014-12-16 16:33:50 +0100

    Workin on it

--- app/src/main/java/com/maddies/arduinobluetoothapp/FileExplore.java
+++ app/src/main/java/com/maddies/arduinobluetoothapp/FileExplore.java
@@ -215,17 +215,26 @@ public class FileExplore extends Activity {
 
     public static byte[] readFile(File file) throws IOException {
         // Open file
+    
         RandomAccessFile f = new RandomAccessFile(file, "r");
         try {
-            // Get and check length
-            long longlength = f.length();
-            int length = (int) longlength;
-            if (length != longlength)
-                throw new IOException("File size >= 2 GB");
-            // Read file and return data
-            byte[] data = new byte[length];
-            f.readFully(data);
-            return data;
+            if(state == 1){
+                ddfdfdf 
+            } else if (state == 2){
+                
+            }
+            else if(state == 3){
+                // Get and check length
+                long longlength = f.length();
+                int length = (int) longlength;
+                if (length != longlength)
+                    throw new IOException("File size >= 2 GB");
+                // Read file and return data
+                byte[] data = new byte[length];
+                f.readFully(data);
+                return data;
+            }
+            return null;
         } finally {
             f.close();
         }
