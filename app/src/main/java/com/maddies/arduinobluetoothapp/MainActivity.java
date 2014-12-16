8b3b0e243bc1b1c1298d4e941284c0684ac41e12
Maarten Enthoven <maarten2200@gmail.com>
2014-12-16 16:28:40 +0100

    Device has no bluetooth (enabled)
    will close app or ask to enable it

--- app/src/main/java/com/maddies/arduinobluetoothapp/MainActivity.java
+++ app/src/main/java/com/maddies/arduinobluetoothapp/MainActivity.java
@@ -1,10 +1,11 @@
 package com.maddies.arduinobluetoothapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
@@ -18,7 +19,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    public static final String LOG = "Bluetooth App";
+    public static final String TAG = "Bluetooth App";
 
     private static final int REQUEST_ENABLE_BT = 1;
     public static ConnectThread bluetoothThread;
@@ -31,45 +32,33 @@ public class MainActivity extends Activity {
         // sets up broadcast receiver for bluetooth state
         IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
         registerReceiver(mReceiver, filter);
-        
-        
+
+
         // searches for arduino devices
         Button searchButton = (Button) findViewById(R.id.search_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
                 // does device have bluetooth
                 BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
                 if (mBluetoothAdapter == null) {
-                    
                     // Device does not support Bluetooth
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setMessage("Your device has no bluetooth")
-                        .setCancelable(false)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                onDestroy();
-                            }
-                        });
-                    
-                    AlertDialog alert = builder.create();
-                    alert.show();
-                    
+
+                    closeAplication("Your device doesn't support Bluetooth.");
+
                 } else {
                     // Device supports bluetooth
-                    Log.i(LOG, "Wow you have bluetooth");
-
 
                     if (!mBluetoothAdapter.isEnabled()) {
                         // bluetooth is not enabled
-                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                        Log.i(LOG, "Enabling Bluetooth");
+
+                        enableBluetooth();
+                        Log.d(TAG, "Enabling Bluetooth");
 
                     } else {
-                        Log.i(LOG, "Bluetooth is enabled");
+                        // bluetooth is enabled
+                        Log.d(TAG, "Bluetooth is enabled");
                     }
                 }
 
@@ -105,21 +94,40 @@ public class MainActivity extends Activity {
         });
     }
 
+
+
+
     // Gets result after enabling bluetooth
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
 
         if (requestCode == 1) {
             if(resultCode == RESULT_OK){
                 // User succesfully enabled bluetooth
-//              String result=data.getStringExtra("result");
-                Log.i(LOG, "User succesfully enabled bluetooth");
+                Log.d(TAG, "User successfully enabled bluetooth");
+                lookForArduino();
             }
             if (resultCode == RESULT_CANCELED) {
                 // User stops the bluetooth enabling process
-                Log.i(LOG, "User stops the bluetooth enabling process");
+                Log.d(TAG, "User stops the bluetooth enabling process");
+
+                closeAplication("The application works only with Bluetooth enabled");
             }
         }
     }
+
+
+    // tries to find an arduino
+    private void lookForArduino() {
+
+    }
+
+    // enables bluetooth
+    private void enableBluetooth() {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+
 
     // listens if user manually disables bluetooth
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -132,30 +140,70 @@ public class MainActivity extends Activity {
                         BluetoothAdapter.ERROR);
                 switch (state) {
                     case BluetoothAdapter.STATE_OFF:
-                        Log.i(LOG, "User turned Bluetooth off");
+                        Log.d(TAG, "User turned Bluetooth off");
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Bluetooth was disabled. Do you want to enable it?")
+                                .setCancelable(false)
+                                .setTitle("Error")
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        enableBluetooth();
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        closeAplication("The application works only with Bluetooth enabled");
+                                    }
+                                });
+
+                        AlertDialog alert = builder.create();
+                        alert.show();
+
                         break;
                     case BluetoothAdapter.STATE_TURNING_OFF:
-                        Log.i(LOG, " User is turning bluetooth off...");
+                        Log.d(TAG, " User is turning bluetooth off...");
                         break;
                     case BluetoothAdapter.STATE_ON:
-                        Log.i(LOG, "User switched bluetooth on");
+                        Log.d(TAG, "User switched bluetooth on");
                         break;
                     case BluetoothAdapter.STATE_TURNING_ON:
-                        Log.i(LOG, "User is turning bluetooth on");
+                        Log.d(TAG, "User is turning bluetooth on");
                         break;
                     default:
-                        Log.i(LOG, "what happened?");
+                        Log.d(TAG, "what happened?");
                 }
             }
         }
     };
 
+    // will exit application but not close if user has bluetooth problems
+    private void closeAplication(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(message + " The application will no close.")
+                .setCancelable(false)
+                .setTitle("Error")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
     @Override
     public void onDestroy() {
-        super.onDestroy();
-
         // closes the broadcast receiver
         unregisterReceiver(mReceiver);
+        super.onDestroy();
 
     }
 
@@ -172,7 +220,7 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        
+
         switch (id){
             // developer button is pressed
             case R.id.developers:
